a0a13e34f480a9aacde14bbee5a6fe01fe6b0baf
EDUSAHE <JOSESAHE1@OUTLOOK.COM>
2023-01-28 10:42:29 -0600

    ProyectoP3-gramas

---
+++ ProyectoP3/n-gramas.py
@@ -0,0 +1,30 @@
+import re
+import itertools
+
+def read_file():
+    with open("Ejemplo.txt", encoding="utf8") as textfile:
+        return [re.sub(r'[0-9=\n]','',i).split(',') for i in textfile if re.sub(r'[0-9=\n]','',i)]
+def write_file(name,string):
+    with open(name,"w", encoding="utf8") as textfile:
+        textfile.write(string)
+        textfile.close()
+        return
+
+def make_ngrama(data,n):
+    return [",".join(data[i:n+i]) for i in range(len(data)-(n-1)) if n<=len(data)]
+
+def count_ngramas(data,n,b,name):
+    ngramas = list(itertools.chain(*map(lambda x: make_ngrama(x,n),data)))
+    h = sorted(filter(lambda f: f[0]>=b,([ngramas.count(i),i] for i in {c for c in ngramas})), key=lambda x: x[0],reverse=True)
+    s = "\n".join(f"[{i[0]}] {i[1]}" for i in h)
+    output = f"""Se encontraron {len(h)} gramas de tamaño {n}
+{s}
+    """
+    write_file(name,output)
+    return
+
+if __name__ == '__main__':
+    l = read_file()
+    count_ngramas(l,1,10,"Ejemplo_n_1_B_10.txt")
+    count_ngramas(l,2,7,"Ejemplo_n_2_B_7.txt")
+    count_ngramas(l,3,4,"Ejemplo_n_3_B_4.txt")
